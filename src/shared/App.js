6f95ede9a0da40cffc962fe09d917490159d76cd
sojh93 <36619557+sojh93@users.noreply.github.com>
2022-05-09 15:10:22 +0900

    0509 #1 5강 전 준비코드 적용
    준비코드 적용

--- src/shared/App.js
+++ src/shared/App.js
@@ -46,6 +46,7 @@ function App() {
           <Route path="/login" exact component={LogIn} />
           <Route path="/signup" exact component={SignUp}/>
           <Route path="/write" exact component={PostWrite}/>
+          <Route path="/write/:id" exact component={PostWrite} />
           <Route path="/post/:id" exact component={PostDetail}/>
           <Route path="/search" exact component={Search}/>
           <Route path="/noti" exact component={Notification} />
